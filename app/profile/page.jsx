fc35f4cd752ed6a482de32e98c174a54f37c21ce
Bernardo <sardinhaber@gmail.com>
2025-03-06 15:55:34 -0300

    refactor: text in cards

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,7 +9,7 @@ export default function Initial() {
                 <img src="https://i.pinimg.com/236x/21/9e/ae/219eaea67aafa864db091919ce3f5d82.jpg"  alt="" />
                 <p>Bernardo Gabriel de Moraes Marques</p>
             </div>
-            <Card title={"Descrição"} text={"Olá meu nome é Bernardo, tenho 16 anos, atualmente estou cursando Desenvolvimento de sistema no Senai Valinhos e também o Ensino Médio no Sesi Valinhos"} />
+            <Card title={"Descrição"} text={"Olá meu nome é Bernardo, tenho 16 anos, atualmente estou cursando Desenvolvimento de sistema no Senai Valinhos e também o Ensino Médio no Sesi Valinhos."} />
             <Card title={"Descrição"} text={"Desenvolvedor Full Stack"} />
             <Card title={"Descrição"} text={"Desenvolvedor Full Stack"} />
             <Card title={"Descrição"} text={"Desenvolvedor Full Stack"} />

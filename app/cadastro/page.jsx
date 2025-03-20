741f18b2d5acaff596dc39453b81aab9a4190985
Bernardo <sardinhaber@gmail.com>
2025-03-20 08:33:34 -0300

    feat: finish project

--- app/cadastro/page.jsx
+++ app/cadastro/page.jsx
@@ -7,40 +7,55 @@ import { useRouter } from "next/navigation"
 
 export default function Cadastro() {
     const router = useRouter();
-    const [password, setPassword] = useState("");
-    const [confirmPass, setConfirmPass] = useState("");
-    const [match, setMatch] = useState(null)
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+    const [match, setMatch] = useState(null);
     const [error, setError] = useState(null);
 
     const handleCheck = () => {
-        if (password === confirmPass && password !== "") {
-        setMatch(false);
-        setError("As senhas não coincidem");
-        } else if (password.length < 8) {
+        if (input1 !== input2) {
             setMatch(false);
-            setError("A senha deve ter no minimo 8 caracteres");
+            setError("As senhas não coincidem");
+        }
+        else if (input1.length === 0) {
+            setMatch(false);
+            setError("Preencha o campo de senha");
+        }
+        else if (input2.length === 0) {
+            setMatch(false);
+            setError("Preencha o campo de repetir senha");
         }
         else {
             setMatch(true);
-            setError("/profile");
-        }
-    }
-
+            setError("sucesso");
+        };
+    };
     return (
         <div className="container">
-        <div className="login">
-            <h1>CADASTRO</h1>
-            <h3>Crie ja sua conta e aproveite ao maximo</h3>
-            <Input tipo="text" text="Username" />
-            <Input id={password} tipo="password" text="Password" onChange={(e) => setPassword(e.target.value)} />
-            <Input id={confirmPass} tipo="password" text="Confirm Password" onChange={(e) => setConfirmPass(e.target.value)} />
-            <button onClick={handleCheck}>Verificar</button>
-            {match !== null && <p></p> }
-            <button className= "button" onClick={() => router.push("/")}>Cadastre-se</button>
-            <nav>
-                <p>Já tem uma conta? <button className="cadastro" onClick={() => router.push("/")}> Faça login</button></p>
-        </nav>
+            <div className="login">
+                <h1>CADASTRO</h1>
+                <h3>Crie ja sua conta e aproveite ao maximo</h3>
+                <Input tipo="text" text="Username" />
+                <input
+                    className="input"
+                    title="Senha:"
+                    value={input1}
+                    placeholder="Password"
+                    onChange={(e) => setInput1(e.target.value)}
+                />
+                <input
+                    className="input"
+                    title="Repita a senha:"
+                    value={input2}
+                    placeholder="Repeat password"
+                    onChange={(e) => setInput2(e.target.value)}
+                />
+                <button  className="button" onClick={handleCheck}>Cadastre-se</button>{" "}
+                {match !== null && <p>{error}</p>}{" "}
+                <nav>
+                    <p>Já tem uma conta? <button className="cadastro" onClick={() => router.push("/")}> Faça login</button></p>
+                </nav>
+            </div>
         </div>
-    </div>
-    ) 
+    )
 }
